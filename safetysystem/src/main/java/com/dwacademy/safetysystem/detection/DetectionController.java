d000b4d573068a3f355a45a524dc4ce69152787f
gunhee-04 <qlwsjdj@gmail.com>
2026-03-20 17:37:12 +0900

    flask 랑 Spring 연결

---
+++ safetysystem/src/main/java/com/dwacademy/safetysystem/detection/DetectionController.java
@@ -0,0 +1,29 @@
+package com.dwacademy.safetysystem.detection;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/detection")
+public class DetectionController {
+
+    @PostMapping
+    public ResponseEntity<String> receiveDetection(@RequestBody DetectionRequestDto request) {
+
+        System.out.println("===== Detection API 호출됨 =====");
+
+        System.out.println("카메라 ID: " + request.getCameraId());
+        System.out.println("이벤트 타입: " + request.getEventType());
+        System.out.println("위험도: " + request.getEventLevel());
+
+        System.out.println("감지 수: " + request.getDetectedCount());
+        System.out.println("체류 시간: " + request.getStayDurationSec());
+
+        System.out.println("메시지: " + request.getMessage());
+        System.out.println("이벤트 시간: " + request.getEventTime());
+
+        System.out.println("===============================");
+
+        return ResponseEntity.ok("success");
+    }
+}
---
+++ safetysystem/src/main/java/com/dwacademy/safetysystem/detection/DetectionRequestDto.java
@@ -0,0 +1,19 @@
+package com.dwacademy.safetysystem.detection;
+
+import lombok.Data;
+
+@Data
+public class DetectionRequestDto {
+
+    private String cameraId;          // 카메라 코드 (CAM-01)
+    private String eventType;         // INTRUSION, CROWD
+    private String eventLevel;        // LOW, MEDIUM, HIGH, CRITICAL
+
+    private Integer detectedCount;    // 감지된 객체 수 (밀집)
+    private Integer stayDurationSec;  // 체류 시간 (침입)
+
+    private String message;           // 이벤트 설명
+    private String image;             // Base64 이미지 (선택)
+
+    private String eventTime;         // 발생 시간 (문자열로 일단 받기)
+}
