f8037b4e6b68a3a7e1c139fc4b57c634afc5a44a
moon <skuco3580@gmail.com>
2026-04-06 12:13:07 +0900

    주의 알림 작업

--- safetysystem/src/main/java/com/dwacademy/safetysystem/detection_event/domain/EventLevel.java
+++ safetysystem/src/main/java/com/dwacademy/safetysystem/detection_event/domain/EventLevel.java
@@ -2,8 +2,6 @@ package com.dwacademy.safetysystem.detection_event.domain;
 
 public enum EventLevel {
     NORMAL,
-//    WARNING,
-//    ALERT,   // 과거 DB 데이터 인식용
     MEDIUM,  // 파이썬 밀집(CROWD) 전송용
     HIGH     // 파이썬 침입(INTRUSION) 전송용
 }
